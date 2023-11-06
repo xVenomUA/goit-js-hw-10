cae466bc1ffbe30e592f94478455371289ea3c61
Pohrebniak Andrian <pogra29@gmail.com>
2023-11-06 20:16:49 +0200

    ff

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,8 +1,8 @@
 import { Notify } from 'notiflix';
 import axios, { Axios } from 'axios';
-
 axios.defaults.headers.common['x-api-key'] =
   'live_bKkP5sdqYhKCDOuk3F9zZUnhE0OowjsU0wDLGPCIIPurZptzPaUesVO0tQ9QBS2v';
-const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
-let response = axios.get('https://api.thecatapi.com/v1/breeds');
-console.log(response);
+function fetchBreeds() { 
+    axios.get('https://api.thecatapi.com/v1/breeds');
+    
+}
