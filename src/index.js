f589fbe2e4f55ea9bae0a7d50a307941c03b874e
Pohrebniak Andrian <pogra29@gmail.com>
2023-11-07 14:35:51 +0200

    ff

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'slim-select/dist/slimselect.css';
 const URL_BREED = 'https://api.thecatapi.com/v1/breeds';
 const URL_SEARCH = 'https://api.thecatapi.com/v1/images/search';
 const BreedSelect = document.querySelector('.breed-select');
+const CatInfo = document.querySelector('.cat-info');
 fetchBreeds(URL_BREED)
   .then(data => {
     const markUpSelect = data
@@ -20,16 +21,20 @@ BreedSelect.addEventListener('change', evt => {
   const IDBreed = evt.currentTarget.value;
   fetchBreeds(URL_SEARCH + `?breed_ids=${IDBreed}`).then(data => {
     console.log(data);
-    const markUpPhoto = data.map(({
-      url,
-      height,
-      width,
-      id,
-      breeds: { 0: {
-        name, origin,
-      } },
-    }) => { 
-      
-    });
+    const markUpPhoto = data
+      .map(
+        ({
+          url,
+          breeds: {
+            0: { name, origin },
+          },
+        }) => `<div id="js-cat">
+  <img src="${url}" alt="${name}" width="${350}" height="${250}">
+  <p>${name}</p>
+  <p>${origin}</p>
+</div>`
+      )
+      .join('');
+    CatInfo.innerHTML = markUpPhoto;
   });
 });
